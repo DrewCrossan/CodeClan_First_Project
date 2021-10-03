6ac9fa5c2deef1f014a0570b69636992f8710fb3
Andrew Crossan <andrewcrossanit@gmail.com>
2021-10-03 23:17:38 +0100

    added in a show route to give country name a clickable link and show its own page via id

--- drews_travel_bucket_list/controllers/country_controller.py
+++ drews_travel_bucket_list/controllers/country_controller.py
@@ -12,3 +12,17 @@ country_blueprint = Blueprint("countries", __name__)
 def countries():
     countries = country_repository.select_all()
     return render_template("countries/index.html", all_countries=countries)
+
+# SHOW
+# GET '/countries/<id>'
+@country_blueprint.route("/countries/<id>")
+def show_country(id):
+    country = country_repository.select(id)
+    return render_template('countries/show.html', country=country)
+
+# DELETE
+# DELETE '/countries/<id>'
+@country_blueprint.route("/countries/<id>/delete", methods=['POST'])
+def delete_country(id):
+    country_repository.delete(id)
+    return redirect('/countries')
