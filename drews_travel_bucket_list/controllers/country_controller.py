bf774d4e09ea629afea749c04b2d208430efcd70
Andrew Crossan <andrewcrossanit@gmail.com>
2021-10-04 13:31:25 +0100

    added an 'add a country' into country index which opens up a form to create a new country. tested and works.

--- drews_travel_bucket_list/controllers/country_controller.py
+++ drews_travel_bucket_list/controllers/country_controller.py
@@ -7,11 +7,28 @@ import repositories.city_repository as city_repository
 country_blueprint = Blueprint("countries", __name__)
 
 # INDEX
-# Get Countries
+# Get '/countries'
 @country_blueprint.route("/countries")
 def countries():
     countries = country_repository.select_all()
     return render_template("countries/index.html", all_countries=countries)
+
+# NEW
+# GET '/books/new,'
+@country_blueprint.route("/countries/new")
+def new_country():
+    return render_template("countries/new.html")
+
+# CREATE
+# POST '/books'
+@country_blueprint.route("/countries", methods=['POST'])
+def create_country():
+    name = request.form['country_name']
+    continent = request.form['continent']
+    visited = request.form['visited']
+    country = Country(name, continent, visited)
+    country_repository.save(country)
+    return redirect('/countries')
 
 # SHOW
 # GET '/countries/<id>'
